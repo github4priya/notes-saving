728bcfbe305788e1d22d82c3fdd6ad853407a2c0
priyanita04 <priya.saps.2000@gmail.com>
2022-06-01 16:20:59 +0530

    proxy added

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
+        const conn = await mongoose.connect("mongodb+srv://priyagupta8000:zsyJrdkhUG31NMnM@cluster0.oi2hk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,87 +1,85 @@
-import { Button, Form, FormControl, Navbar, Nav, NavDropdown } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
-import {useNavigate} from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
-import { logout } from '../../actions/userActions'
-import { useEffect } from 'react'
-
+import {
+  Button,
+  Form,
+  FormControl,
+  Navbar,
+  Nav,
+  NavDropdown,
+} from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../../actions/userActions";
+import { useEffect } from "react";
 
 const Header = () => {
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-    const userLogin = useSelector((state) => state.userLogin);
-    const { userInfo } = userLogin;
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
 
-    useEffect(() => {
-        if(!userInfo)
-        {
-            navigate('/');
-        }
-    }, [userInfo]);
-
-
-    const logoutHandler = () => {
-        dispatch(logout());
-        // navigate('/');
+  useEffect(() => {
+    if (!userInfo) {
+      navigate("/");
     }
+  }, [userInfo]);
 
-    return (
-        <Navbar bg="light" expand="lg">
-                <Navbar.Brand>
-                    <Link to='/'> Notes Saving</Link>
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">
-                        {
-                            userInfo?(
-                                <>
-                                    <Nav.Link>
-                                        <Link to='/mynotes'>My Notes</Link>
-                                    </Nav.Link>
-                                </>
-                            ):
-                            (
-                                <>
-                                    <Nav.Link>
-                                        <Link to='/login'>Login</Link>
-                                    </Nav.Link>
-                                </>
-                            )
-                        }
+  const logoutHandler = () => {
+    dispatch(logout());
+    // navigate('/');
+  };
 
-                        {
-                            userInfo?(
-                                <>
-                                    <NavDropdown title="Settings" id="basic-nav-dropdown">
-                                    <NavDropdown.Item href="/myprofile">My Profile</NavDropdown.Item>
-                                    <NavDropdown.Divider />
-                                    <NavDropdown.Item
-                                        onClick={logoutHandler}
-                                    >Logout</NavDropdown.Item>
-                                    </NavDropdown>
-                                </>
-                            )
-                            : (
-                                <>
-                                    <Nav.Link>
-                                        <Link to='/register'>Register</Link>
-                                    </Nav.Link>
-                                </>
-                            )
-                        }
+  return (
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand>
+        <Link to="/"> Notes Saving</Link>
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          {userInfo ? (
+            <>
+              <Nav.Link>
+                <Link to="/mynotes">My Notes</Link>
+              </Nav.Link>
+            </>
+          ) : (
+            <>
+              <Nav.Link>
+                <Link to="/login">Login</Link>
+              </Nav.Link>
+            </>
+          )}
 
-                    </Nav>
+          {userInfo ? (
+            <>
+              <NavDropdown title="Settings" id="basic-nav-dropdown">
+                <NavDropdown.Item href="/myprofile">
+                  My Profile
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item onClick={logoutHandler}>
+                  Logout
+                </NavDropdown.Item>
+              </NavDropdown>
+            </>
+          ) : (
+            <>
+              <Nav.Link>
+                <Link to="/register">Register</Link>
+              </Nav.Link>
+            </>
+          )}
+        </Nav>
 
-                    {/* <Form >
+        {/* <Form >
                         <FormControl type="text" placeholder="Search" className="mr-sm-2"/>
                         <Button variant="outline-success">Search</Button>
                     </Form> */}
-
-                </Navbar.Collapse>
-        </Navbar>
-    );
-}
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
 
 export default Header;
